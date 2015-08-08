3c08d0a7828a622e0eca1f2d749e17966409d625
Krishna <barathkrshna@gmail.com>
2015-08-08 11:27:16 +0530

    going to add a new file

---
+++ Test1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Test1
+{
+public static void main(String args[])
+{
+System.out.println(“I m testing git software”);
+System.out.println("oh i m working on git");
+System.out.println("i m going to add a new file");
+}
+}
